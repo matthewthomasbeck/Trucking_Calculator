312e7054cecb4381c287d84eb0651313e8023105
matthewthomasbeck <matthewthomasbeck@gmail.com>
2024-02-27 10:26:38 -0600

    Small updates

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Trucking_Calculator
 
-Under the instruction of Dr. Mauricio Gomez at the University of Texas at San Antonio in his Programming I lecture, I created a program used to read the average miles driven by a truck driver for every day of the week. It will then start asking the value of miles driven for Monday and will continue asking the values until getting the value corresponding to Sunday. The program will then print the table shown above based on the values input by the user.
+Under the instruction of Dr. Mauricio Gomez at the University of Texas at San Antonio in his Programming I lecture during the Spring 2024 semester, I created a program used to read the average miles driven by a truck driver for every day of the week. It will then start asking the value of miles driven for Monday and will continue asking the values until getting the value corresponding to Sunday. The program will then print the table shown above based on the values input by the user.
--- src/Trucking.java
+++ src/Trucking.java
@@ -1,11 +1,11 @@
-/**********************************************************************************/
-/* This code and its associated files were created at the instruction of          */
-/* professors at the University of Texas at San Antonio during my time as a       */
-/* student at the university. I, as a student, was not responsible for the idea   */
-/* behind this code (i.e. project guidelines, functionality, and end purpose),    */
-/* but I, Matthew Thomas Beck, can confirm that myself and any project partners   */
-/* (if applicable) were the ones responsible for writing it.                      */
-/**********************************************************************************/
+/********************************************************************************/
+/* This code and its associated files were created at the instruction of        */
+/* professors at the University of Texas at San Antonio during my time as a     */
+/* student at the university. I, as a student, was not responsible for the idea */
+/* behind this code (i.e. project guidelines, functionality, and end purpose),  */
+/* but I, Matthew Thomas Beck, can confirm that myself and any project partners */
+/* (if applicable) were the ones responsible for writing it.                    */
+/********************************************************************************/
 
 
 
@@ -43,8 +43,6 @@ public class Trucking { // main class to be used to take user input and then dis
     /********** MAIN FUNCTION **********/
 
     public static void main(String[] args) { // main function to be called
-
-        /***** create dependencies *****/
 
         /***** initial print *****/
 
@@ -218,7 +216,8 @@ public class Trucking { // main class to be used to take user input and then dis
 
             for (j = 0; j < 7; j++) { // loop for 7 days
 
-                if (currentDate <= 0 || currentDate > 30) { // if current date negative (month has not started yet) or out of range...
+                // if current date negative (month has not started yet) or out of range...
+                if (currentDate <= 0 || currentDate > 30) {
 
                     System.out.print(String.format("\t\t0-0\t")); // print nothing
 
